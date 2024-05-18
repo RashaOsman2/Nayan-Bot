b97d489f5663809208b21e85f7e99ff5ccc8aa16
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-05-18 21:02:03 +0600

    Update Bedi.js

--- scripts/commands/Bedi.js
+++ scripts/commands/Bedi.js
@@ -1,27 +1,27 @@
- const fs = require("fs");
+const fs = require("fs");
 module.exports.config = {
     name: "women",
     version: "1.0.0",
-    permssion: 0,
+    permission: 0,
     credits: "Rasha",
     description: "emoji",
-  prefix: true,
+    prefix: true,
     category: "Box chat",
-    usages: "< link/UID >",
+    usages: "<link/UID>",
     cooldowns: 5
 };
 
 module.exports.handleEvent = function({ api, event }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("women")==0 || event.body.indexOf("Women")==0 || event.body.indexOf("☕")==0 || event.body.indexOf("Bedi")==0) {
-		var msg = {
-				body: "English: Women ☕ Filipino: Babae☕ Bangla: মহিলা ☕ Hindi: महिला ☕ Urdu: خاتون ☕ Arabic: نساء ☕ Chinese: 女性 ☕ Croatian: žena ☕ Japanese: 女性 ☕ Spanish: Mujer ☕ Danish: Kvinde ☕ Italian: Donna ☕ German: Frau ☕ Dutch: Vrouw ☕ Finnish: Nainen ☕ French: Femme ☕ Greek: γυναίκα ☕ Portuguese: Mulher ☕ Belarusian: жанчыны ☕ korean :Namja☕",
-				attachment: fs.createReadStream(__dirname + `/`)
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("☕", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+    var { threadID, messageID } = event;
+    if (event.body.toLowerCase().indexOf("women") === 0 || event.body.indexOf("☕") === 0 || event.body.indexOf("Bedi") === 0) {
+        var msg = {
+            body: "English: Women ☕ Filipino: Babae ☕ Bangla: মহিলা ☕ Hindi: महिला ☕ Urdu: خاتون ☕ Arabic: نساء ☕ Chinese: 女性 ☕ Croatian: žena ☕ Japanese: 女性 ☕ Spanish: Mujer ☕ Danish: Kvinde ☕ Italian: Donna ☕ German: Frau ☕ Dutch: Vrouw ☕ Finnish: Nainen ☕ French: Femme ☕ Greek: γυναίκα ☕ Portuguese: Mulher ☕ Belarusian: жанчыны ☕ Korean: 여성 ☕"
+        };
+        api.sendMessage(msg, threadID, messageID);
+        api.setMessageReaction("☕", event.messageID, (err) => {}, true);
+    }
+};
 
-  }
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+    // No implementation needed for the run function in this case
+};
