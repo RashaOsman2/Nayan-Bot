bac841b453297918a637beef3a14a8e14b0cbb85
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-05-18 11:16:57 +0600

    Create Bedi.js

---
+++ scripts/commands/Bedi.js
@@ -0,0 +1,27 @@
+ const fs = require("fs");
+module.exports.config = {
+    name: "women",
+    version: "1.0.0",
+    permssion: 0,
+    credits: "Rasha",
+    description: "emoji",
+  prefix: true,
+    category: "Box chat",
+    usages: "< link/UID >",
+    cooldowns: 5
+};
+
+module.exports.handleEvent = function({ api, event }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("women")==0 || event.body.indexOf("Women")==0 || event.body.indexOf("☕")==0 || event.body.indexOf("Bedi")==0) {
+		var msg = {
+				body: "English: Women ☕ Filipino: Babae☕ Bangla: মহিলা ☕ Hindi: महिला ☕ Urdu: خاتون ☕ Arabic: نساء ☕ Chinese: 女性 ☕ Croatian: žena ☕ Japanese: 女性 ☕ Spanish: Mujer ☕ Danish: Kvinde ☕ Italian: Donna ☕ German: Frau ☕ Dutch: Vrouw ☕ Finnish: Nainen ☕ French: Femme ☕ Greek: γυναίκα ☕ Portuguese: Mulher ☕ Belarusian: жанчыны ☕ korean :Namja☕",
+				attachment: fs.createReadStream(__dirname + `/`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("☕", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
