670acdad6e5504dcd538556b76a41f0671081877
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-05-20 19:35:13 +0600

    Create Oree.js

---
+++ scripts/commands/Oree.js
@@ -0,0 +1,27 @@
+ const fs = require("fs");
+module.exports.config = {
+    name: "oree",
+    version: "1.0.0",
+    permssion: 0,
+    credits: "Rasha",
+    description: "emoji",
+  prefix: true,
+    category: "Box chat",
+    usages: "< link/UID >",
+    cooldowns: 5
+};
+
+module.exports.handleEvent = function({ api, event }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Ore")==0 || event.body.indexOf("Oreh")==0 || event.body.indexOf("Osthir")==0 || event.body.indexOf("osthir")==0) {
+		var msg = {
+				body: "Orehhh Osthirrr",
+				attachment: fs.createReadStream(__dirname + `/Nayan/b217-156f-48aa-a521-5a790a24294b.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😝", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
