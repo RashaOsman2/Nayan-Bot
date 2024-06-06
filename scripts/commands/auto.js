7c17ca639762cf3f43062f2e6b5dfff16bb5a1b7
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-06-07 01:14:44 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -1,5 +1,5 @@
 module.exports = {
-  config: {
+  config:{
     name: "auto",
     version: "0.0.2",
     permission: 0,
@@ -9,31 +9,31 @@ module.exports = {
     category: "user",
     usages: "",
     cooldowns: 5,
-  },
-  start: async function ({ nayan, events, args }) {},
-  handleEvent: async function ({ api, event, args }) {
-    const axios = require("axios");
-    const request = require("request");
-    const fs = require("fs-extra");
-    const content = event.body ? event.body : '';
-    const body = content.toLowerCase();
-    const { alldown } = require("nayan-media-downloader");
+},
+start: async function({ nayan, events, args }) {},
+handleEvent: async function ({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const {alldown} = require("nayan-media-downloader")
+  if (body.startsWith("https://")) {
+  api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
+const data = await alldown(content);
+  console.log(data)
+  const {low, high, title} = data.data;
+    api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
+  const video = (await axios.get(high, {
+      responseType: "arraybuffer",
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
     
-    if (body.startsWith("https://") || body.includes("https://www.facebook.com")) {
-      api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
-      const data = await alldown(content);
-      console.log(data);
-      const { low, high, title } = data.data;
-      api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
-      const video = (await axios.get(high, {
-        responseType: "arraybuffer",
-      })).data;
-      fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"));
+        return api.sendMessage({
+            body: `《TITLE》: ${title}`,
+            attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
-      return api.sendMessage({
-        body: `《TITLE》: ${title}`,
-        attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
-      }, event.threadID, event.messageID);
+        }, event.threadID, event.messageID);
     }
-  }
+}
 }
